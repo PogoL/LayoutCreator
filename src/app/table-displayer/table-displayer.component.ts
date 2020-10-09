4c66442ee41baabe4c828392ca41cee462f0f7a6
pogorzelczykp <patryk.pogorzelczyk@snpgroup.com>
2020-10-09 09:45:15 +0200

    add table-displayer module

---
+++ src/app/table-displayer/table-displayer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-displayer',
+  templateUrl: './table-displayer.component.html',
+  styleUrls: ['./table-displayer.component.css']
+})
+export class TableDisplayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
