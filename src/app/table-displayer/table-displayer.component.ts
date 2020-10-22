c77efc4fa0bfef25d4e92156cc4127b70b7e8e6c
PogoL <patryk.pogorzelczyk@gmail.com>
2020-10-22 16:24:14 +0200

    create simple table on init

--- src/app/table-displayer/table-displayer.component.ts
+++ src/app/table-displayer/table-displayer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TableModel } from '../table.model';
 
 @Component({
   selector: 'app-table-displayer',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableDisplayerComponent implements OnInit {
   
+  table: TableModel = new TableModel(4,5);
+  
   constructor() { }
 
   ngOnInit(): void {
+    let divTable = document.getElementById("divTable");
+    let table = document.createElement("table");
+    for (let idH = 0; idH < this.table.height; idH++) {
+      let tr = document.createElement("tr");
+      for (let idW = 0; idW < this.table.width; idW++) {
+        let td = document.createElement("td");
+        td.setAttribute('class', 'empty-cell');
+        tr.appendChild(td);
+      }
+      table.appendChild(tr);
+    }
+
+    divTable.appendChild(table);
   }
+
 
 }
