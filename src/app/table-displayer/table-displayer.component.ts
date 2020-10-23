2eda9e7ce66a01190b461ffd808689a48df5c35a
PogoL <patryk.pogorzelczyk@gmail.com>
2020-10-23 13:50:06 +0200

    merge cells with deleting from selected

--- src/app/table-displayer/table-displayer.component.ts
+++ src/app/table-displayer/table-displayer.component.ts
@@ -35,4 +35,42 @@ export class TableDisplayerComponent implements OnInit {
       this.renderer.addClass(event.target, 'selected-cell');
     }
   }
+
+  mergeCells(): void {
+    this.selectedCells.sort(
+      function(a, b) {          
+        if (a.idHeight === b.idHeight) {
+          return a.idWidth - b.idWidth;
+        }
+        return a.idHeight > b.idHeight ? 1 : -1;
+      });
+    
+    console.table(this.selectedCells);
+    
+
+    let importantCell = document.getElementById('td'+this.selectedCells[0].idHeight+this.selectedCells[0].idWidth);
+    let firstCell = this.selectedCells[0];
+    let lastCell = this.selectedCells[this.selectedCells.length-1];
+    let rowspan = Math.abs(firstCell.idHeight-lastCell.idHeight) + 1;
+    let colspan = Math.abs(firstCell.idWidth-lastCell.idWidth) + 1;
+    if (rowspan > 1) {
+      importantCell.setAttribute('rowspan', rowspan.toString());
+    }
+    if (colspan > 1) {
+      importantCell.setAttribute('colspan', colspan.toString());
+    }
+    
+    this.removeElementsByClass('selected-cell');
+    this.selectedCells = [];
+  }
+
+  removeElementsByClass(className: string): void{
+    var elems = document.querySelectorAll('.'+className);
+    [].forEach.call(elems, function(el) {
+      el.classList.remove(className);
+    });
+    elems.forEach((el) => {
+      if (el !== elems[0]) el.remove();
+    });
+  }
 }
